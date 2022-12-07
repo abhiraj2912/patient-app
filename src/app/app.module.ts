bb859c555103d5ac1709ab1a040706e6ed36ffd6
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-07 08:36:53 +0530

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ViewPatientComponent } from './view-patient/view-patient.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 const myRoute:Routes=[
   {
@@ -15,7 +16,7 @@ const myRoute:Routes=[
     component:AddPatientComponent
   },
   {
-    path:"",
+    path:"view",
     component:ViewPatientComponent
   }
 ]
@@ -31,7 +32,8 @@ const myRoute:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/view-patient/view-patient.component.ts
+++ src/app/view-patient/view-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-patient',
@@ -7,7 +8,13 @@ import { Component } from '@angular/core';
 })
 export class ViewPatientComponent {
 
-
+  constructor(private api:ApiService){
+    api.fetchPatient().subscribe(
+      (response)=>{
+        this.data=response
+      }
+    )
+  }
   
   data:any=[]
 

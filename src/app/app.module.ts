2311a61a9ac3999391b2c199803f06f5c3951fb4
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-07 08:26:40 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
+import { ViewPatientComponent } from './view-patient/view-patient.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddPatientComponent
+  },
+  {
+    path:"",
+    component:ViewPatientComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPatientComponent,
+    ViewPatientComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
